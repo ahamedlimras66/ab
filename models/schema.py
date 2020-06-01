9f9f5e3a8849f80f5317ddbdfa31bdebe705259b
mmblack4 <mm4.muthumani@gmail.com>
2020-06-01 22:11:53 +0530

    new db

--- models/schema.py
+++ models/schema.py
@@ -4,24 +4,28 @@ from flask_login import UserMixin
 from sqlalchemy import ForeignKey
 
 
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    town_city = db.Column(db.String(100), nullable=True)
+    address = db.Column(db.String(100), nullable=True)
+    phone = db.Column(db.String(10), nullable=True)
+    invoice = db.relationship('Invoice',cascade="all,delete",backref="customer")
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     price =  db.Column(db.Integer, nullable=False)
-    bill = db.relationship('Bill',cascade="all,delete",backref="product")
+    iteam = db.relationship('Iteam',cascade="all,delete",backref="product")
 
-class Customer(db.Model):
+class Invoice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    town_city = db.Column(db.String(100), nullable=False)
-    address = db.Column(db.String(100), nullable=False)
-    phone = db.Column(db.String(10), nullable=False)
-    bill = db.relationship('Bill',cascade="all,delete",backref="customer")
+    customer_id = db.Column(db.Integer, ForeignKey('customer.id'))
+    invoice_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    iteam = db.relationship('Iteam',cascade="all,delete",backref="invoice")
 
-
-class Bill(db.Model):
+class Iteam(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    invoice_no = db.Column(db.Integer, nullable=False)
-    customer_id = db.Column(db.Integer, ForeignKey('product.id'))
-    product_id = db.Column(db.Integer, ForeignKey('customer.id'))
-    bill_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    invoiceid = db.Column(db.Integer, ForeignKey('invoice.id'))
+    productid = db.Column(db.Integer, ForeignKey('product.id'))
+    quantity = db.Column(db.Integer)
